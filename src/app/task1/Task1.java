f81e7e7a5f3e85c05675668391a5d253fe169e43
vnovikov <nik.hbb@gmail.com>
2024-02-09 04:54:00 +0200

    Add task 1

--- src/app/task1/Task1.java
+++ src/app/task1/Task1.java
@@ -1,7 +1,33 @@
 package app.task1;
 
+import java.util.Arrays;
+
 public class Task1 {
-    public void run(){
-        System.out.println("\n>> print task 1");
+    public void run() {
+        String[] srcArr = {"brange", "plum", "tomato", "onibn", "grape"};
+
+        System.out.println("\nTASK 1\nOutput source array:");
+        printSrcArr(srcArr);
+
+        System.out.println("Output correct array:");
+        printCorrectArr(correctMistake(srcArr));
     }
+
+    private String[] correctMistake(String[] arr) {
+        arr[0] = "orange";
+        arr[3] = "onion";
+        return arr;
+    }
+
+    private void printSrcArr(String[] arr) {
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private void printCorrectArr(String[] correctArr) {
+        for (int i = 0, j=1; i < correctArr.length; i++, j++) {
+            System.out.println(j + ". " + correctArr[i]);
+        }
+    }
+
+
 }
